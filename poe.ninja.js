9cf7e92e9bddcc4fb7f90a7097db71f265f06a8f
jason2080x <jason2080x@outlook.com>
2022-02-08 04:31:35 +0800

    commit 2

---
+++ poe.ninja.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+    const script = document.createElement("script");
+    script.src='https://cdn.jsdelivr.net/npm/jquery@3.4.1/dist/jquery.min.js'
+    document.body.appendChild(script)
+});
